7aa9379e6c3ba8a07b72e1b0f8165c898001158e
sriharshaklu <57980261+sriharshaklu@users.noreply.github.com>
2019-11-21 12:59:19 +0530

    Add files via upload

---
+++ j.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class j
+{
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
